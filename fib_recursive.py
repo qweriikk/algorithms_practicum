3953de931456f0cbc72c7fa2670778bf0edc5a03
qweriikk <dashakots13@gmail.com>
2024-12-26 00:09:17 +0300

    ispravlenie oshibok

--- fib_recursive.py
+++ fib_recursive.py
@@ -1,6 +1,16 @@
+import time
+
 def fib_recursive(n):
     if n <= 1:
         return n
     return fib_recursive(n - 1) + fib_recursive(n - 2)
 
-print(fib_recursive(6))  # 8
+def run_fib_recursive():
+    for n in [5, 10, 15, 20, 24]:
+        start_time = time.time()
+        result = fib_recursive(n)
+        elapsed_time = (time.time() - start_time) * 1000 
+        print(f"fib({n}) = {result}, Time: {elapsed_time:.2f} ms")
+    print("Сложность алгоритма: экспоненциальная (O(2^n)).")
+
+run_fib_recursive()
