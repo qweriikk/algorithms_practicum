8bfd5dd90c4aebd185bbf9bce6662418d9d9655b
qweriikk <dashakots13@gmail.com>
2024-12-26 00:47:31 +0300

    jj

--- fib_big_even_odd.py
+++ fib_big_even_odd.py
@@ -1,6 +1,6 @@
 def fib_eo(n):
     a, b = 0, 1
-    for _ in range(n % 60):  # Повторение чисел Фибоначчи по модулю 10 каждые 60 чисел
+    for _ in range(n % 60):  
         a, b = b, (a + b) % 10
     return "even" if a % 2 == 0 else "odd"
 
