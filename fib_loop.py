9cdb2b84d42b592ee21c56ebd1f73112cd9198b7
qweriikk <dashakots13@gmail.com>
2024-12-26 01:14:13 +0300

    dd

--- fib_loop.py
+++ fib_loop.py
@@ -1,7 +1,16 @@
+import time
+
 def fib(n):
     a, b = 0, 1
     for _ in range(n):
         a, b = b, a + b
     return a
 
-print(fib(7))  # 13
+def run_fib():
+    for n in [5, 10, 15, 20, 32]:
+        start_time = time.time()
+        result = fib(n)
+        elapsed_time = (time.time() - start_time) * 1000  # Время в миллисекундах
+        print(f"fib({n}) = {result}, Time: {elapsed_time:.2f} ms")
+
+run_fib()
